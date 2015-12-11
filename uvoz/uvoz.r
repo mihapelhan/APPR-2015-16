1e67058f4ab375fe184473b0d4ee91058211d66d
Miha Pelhan <miha.pelhan@gmail.com>
2015-12-11 16:54:46 +0100

    nekoliko popravljena tabela

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -6,3 +6,10 @@ url<-'https://en.wikipedia.org/wiki/2014%E2%80%9315_NBA_season'
 
 stran <- html_session(url) %>% read_html(encoding = "UTF-8")
 tabela <- stran %>% html_nodes(xpath ="//table[5]") %>% .[[1]] %>% html_table()
+
+podatki<-read.csv("podatki.csv", header=TRUE, sep=",", dec=".", stringsAsFactors = FALSE, na.strings = ".")
+podatki[,6:30]<-as.numeric(podatki[,6:30])
+a<-6:30
+podatki[a]<-lapply(podatki[a], as.numeric)
+data<-podatki[!is.na(podatki$PTS),]
+data$Rk<-NULL
