0f412a37137e141ba690da6ab40d6cbd21c2c405
Miha Pelhan <miha.pelhan@gmail.com>
2015-12-20 14:02:22 +0100

    dodan graf

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -28,8 +28,14 @@ celatabela<-inner_join(podatki, salaries, by = "Player")
 celatabela$eFG.<-NULL
 colnames(celatabela)<-c("Player","Position" ,"Age","Team","Games","Started","Minutes","FG Made", "FG Att","FG %","3Pt Made", "3Pt Att", "3Pt %", "2Pt Made", "2Pt Att", "2Pt %", "FT Made", "FT Att", "FT %", "Off. Reb", "Def. Reb", "Tot. Reb", "Assists", "Steals", "Blocks", "Turnovers", "Fouls", "Points", "Salary")
 celatabela$Salary<-as.factor(celatabela$Salary)
-celatabela$Salary<-gsub("$","",as.character(celatabela$Salary))
+celatabela$Salary<-gsub("\\$", "", celatabela$Salary)
+celatabela$Salary<-gsub("\\,", "", celatabela$Salary)
+celatabela$Salary<-as.numeric(celatabela$Salary)
 
 require(ggplot2)
 
-graf<-ggplot(data=celatabela %>% filter(Points>1000), aes(x=Points, y=Salary, color=Player, size=(Salary)/(100*Points) )) + guides(color=guide_legend(ncol=2)) + geom_point()
+graf1<-ggplot(data=celatabela %>% filter(Points>1000), aes(x=Points, y=Minutes, color=Points/Minutes))+geom_point()+ geom_text(aes(label=Player),hjust=0, vjust=0)+scale_color_gradient(low="green",high="red")
+print(graf1)
+
+graf2<-ggplot(data=celatabela %>% filter(Points>1250), aes(x=Points, y=Salary, color=(Points*100)/Salary))+geom_point()+geom_text(aes(label=Player))+scale_color_gradient(low="green", high="red")
+print(graf2)
