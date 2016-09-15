cc9e5daa6b2778b12242c00856b91cc3f09001a7
Miha Pelhan <miha.pelhan@gmail.com>
2016-09-15 17:05:18 +0200

    Dokončan prvi graf, dodani grafi po igralnih položajih

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -88,4 +88,25 @@ ekipe<-preuredi(ekipe,zda,'STATE_NAME')
 map2<-map+geom_point(data=ekipe,color='red',size=2,aes(x=LONG,y=LAT)) + geom_text_repel(data=ekipe,color='black',aes(x=LONG,y=LAT,label=Team),size=5)
 map2
 
-#graf1 <- ggplot(celatabela$Points, celatabela$Salary, main="Število točk glede na plačo", xlab="Točke", ylab="Plača")
+datagraf1 <- celatabela[which(celatabela$Points > 1500), ]
+qplot(datagraf1$Points,datagraf1$Salary,color=datagraf1$Team,size=(datagraf1$Minutes),xlab = "Points",ylab = "Salary",main = "Points achieved per dollar") + geom_text_repel(aes(label=datagraf1$Player),size=5, color="black")
+
+dataSG <- celatabela[which(celatabela$Position=="SG"), ]
+dataSG1 <- dataSG[which(dataSG$Points > mean(dataSG$Points)), ]
+qplot(dataSG1$Points,dataSG1$Player,xlab = "Points",ylab = "Player") + geom_vline(xintercept = mean(dataSG1$Points), color="red")
+
+dataC <- celatabela[which(celatabela$Position == "C"), ]
+dataC1 <- dataC[which(dataC$Points > mean(dataC$Points)), ]
+qplot(dataC1$Points,dataC1$Player,xlab = "Points",ylab = "Player") + geom_vline(xintercept = mean(dataC1$Points), color="red")
+
+dataPG <- celatabela[which(celatabela$Position == "PG"), ]
+dataPG1 <- dataPG[which(dataPG$Points > mean(dataPG$Points)), ]
+qplot(dataPG1$Points,dataPG1$Player,xlab = "Points",ylab = "Player") + geom_vline(xintercept = mean(dataPG1$Points), color="red")
+
+dataSF <- celatabela[which(celatabela$Position=="SF"), ]
+dataSF1 <- dataSF[which(dataSF$Points > mean(dataSF$Points)), ]
+qplot(dataSF1$Points,dataSF1$Player,xlab = "Points",ylab = "Player") + geom_vline(xintercept = mean(dataSF1$Points), color="red")
+
+dataPF <- celatabela[which(celatabela$Position=="PF"), ]
+dataPF1 <- dataPF[which(dataPF$Points > mean(dataPF$Points)), ]
+qplot(dataPF1$Points,dataPF1$Player,xlab = "Points",ylab = "Player") + geom_vline(xintercept = mean(dataPF1$Points), color="red")
